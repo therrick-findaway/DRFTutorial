d122350656632f29f5672ce66ac41ca8b926b8fd
therrick-findaway <therrick@MBP13-TH.local>
2021-07-13 12:13:36 -0400

    Refactored ro viewsets and routers

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,20 +3,19 @@ from snippets.models import Snippet
 from django.contrib.auth.models import User
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
 
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style',
-                  'owner']
 
-
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, 
-                                                  queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
